c090d8be63e7fc2634e7bf6b4e5b79987e9a2de5
leleman79 <leleman79@gmail.com>
2021-09-11 10:22:13 -0300

    Primer css

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
+const publicPathFolder = path.resolve(__dirname, './public');
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPathFolder));
 
 app.listen(3000, ()=> {
     console.log('Servidor corriendo en el puerto 3000');
